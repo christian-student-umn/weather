91d7ca7926ed6d9eaca742975a6531dd151ba652
christian-tantheo <christian.tantheo@student.umn.ac.id>
2025-12-04 11:07:08 +0700

    pow

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from model_utils import load_prediction_model, load_scaler, make_prediction
+from model import load_prediction_model, load_scaler, make_prediction
 
 # --- Page Config ---
 st.set_page_config(
